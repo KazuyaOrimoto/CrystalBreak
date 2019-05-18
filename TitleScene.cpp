caba85b29d5676ccd2ab22fc4f32dc4b212a8083
o0kazuya0o <o0kazuya0o@ezweb.ne.jp>
2019-05-18 11:44:05 +0900

    タイトルシーンのインデントをスペースに置き換え

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -17,16 +17,16 @@
 //-----------------------------------------------------------------------------//
 TitleScene::TitleScene()
 {
-	inputManager = InputManager::GetInstance();
-	fileManager = FileManager::GetInstance();
-	titleGraph = fileManager->GetTitleImage();
-	pushGraph = fileManager->GetPushImage();
-	demoPlayMovie = fileManager->GetDemoPlayMovie();
-	changingOpacityValue = 1;
-	SceneChangeFlag = false;
-	playMovie = false;
-	playcount = 0;
-	opacity = 0;
+    inputManager = InputManager::GetInstance();
+    fileManager = FileManager::GetInstance();
+    titleBackGroundGraph = fileManager->GetTitleImage();
+    pushGraph = fileManager->GetPushImage();
+    demoPlayMovie = fileManager->GetDemoPlayMovie();
+    changingOpacityValue = 1;
+    SceneChangeFlag = false;
+    playMovie = false;
+    playcount = 0;
+    opacity = 0;
 }
 
 //-----------------------------------------------------------------------------//
@@ -34,7 +34,7 @@ TitleScene::TitleScene()
 //-----------------------------------------------------------------------------//
 TitleScene::~TitleScene()
 {
-	DeleteGraph(titleGraph);
+    DeleteGraph(titleBackGroundGraph);
 }
 
 //-----------------------------------------------------------------------------//
@@ -42,28 +42,27 @@ TitleScene::~TitleScene()
 //-----------------------------------------------------------------------------//
 SceneBase* TitleScene::Update()
 {
-	inputManager->Update();								//キーの入力情報の更新
-
-	if (inputManager->PushStartButton())
-	{
-		return new TutorialScene();
-	}
-
-	GraphAnimation();									//画像のアニメーション
-
-	needPlayingMovie = inputManager->PushAnyKey();
-
-	if (needPlayingMovie)
-	{
-		PlayingMovie();
-	}
-	else
-	{
-		playcount = 0;
-	}
-
-	return this;
-
+    inputManager->Update();								//キーの入力情報の更新
+    
+    if (inputManager->PushStartButton())
+    {
+        return new TutorialScene();
+    }
+    
+    GraphAnimation();									//画像のアニメーション
+    
+    needPlayingMovie = inputManager->PushAnyKey();
+    
+    if (needPlayingMovie)
+    {
+        PlayMovie();
+    }
+    else
+    {
+        playcount = 0;
+    }
+    
+    return this;
 }
 
 //-----------------------------------------------------------------------------//
@@ -71,17 +70,17 @@ SceneBase* TitleScene::Update()
 //-----------------------------------------------------------------------------//
 void TitleScene::Draw()
 {
-	if (playMovie)
-	{
-		DrawGraph(0,0, demoPlayMovie,FALSE);
-	}
-	else
-	{
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, ALPHA_MAX);
-		DrawGraph(0, 0, titleGraph, TRUE);
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, ALPHA_MAX - opacity);
-		DrawGraph(0, 0, pushGraph, TRUE);
-	}
+    if (playMovie)
+    {
+        DrawGraph(0,0, demoPlayMovie,FALSE);
+    }
+    else
+    {
+        SetDrawBlendMode(DX_BLENDMODE_ALPHA, ALPHA_MAX);
+        DrawGraph(0, 0, titleBackGroundGraph, TRUE);
+        SetDrawBlendMode(DX_BLENDMODE_ALPHA, ALPHA_MAX - opacity);
+        DrawGraph(0, 0, pushGraph, TRUE);
+    }
 }
 
 //-----------------------------------------------------------------------------//
@@ -89,35 +88,35 @@ void TitleScene::Draw()
 //-----------------------------------------------------------------------------//
 void TitleScene::GraphAnimation()
 {
-	opacity += changingOpacityValue;
-	bool switchFlag = (opacity < 0 || OPACITY_MAX < opacity);		//不透明度が0より小さいor上限より大きい
-	if (switchFlag)
-	{
-		changingOpacityValue *= -1;
-	}
+    opacity += changingOpacityValue;
+    bool switchFlag = (opacity < 0 || OPACITY_MAX < opacity);       //不透明度が0より小さいor上限より大きい
+    if (switchFlag)
+    {
+        changingOpacityValue *= -1;
+    }
 }
 
 //-----------------------------------------------------------------------------//
 //						デモプレイ動画を流す
 //-----------------------------------------------------------------------------//
-void TitleScene::PlayingMovie()
+void TitleScene::PlayMovie()
 {
-	playcount++;
-	if (playcount > playMovieUpToTime)
-	{
-		playMovie = true;
-	}
-	else
-	{
-		playMovie = false;
-	}
-
-	if (playMovie)
-	{
-		PlayMovieToGraph(demoPlayMovie);
-	}
-	else
-	{
-		SeekMovieToGraph(demoPlayMovie, 0);							//再生開始位置に戻す
-	}
+    playcount++;
+    if (playcount > playMovieUpToTime)
+    {
+        playMovie = true;
+    }
+    else
+    {
+        playMovie = false;
+    }
+    
+    if (playMovie)
+    {
+        PlayMovieToGraph(demoPlayMovie);
+    }
+    else
+    {
+        SeekMovieToGraph(demoPlayMovie, 0);                         //再生開始位置に戻す
+    }
 }
--- TitleScene.h
+++ TitleScene.h
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------//
-//			リファクタリング：未					チェック：未
+//          リファクタリング：未                  チェック：未
 //-----------------------------------------------------------------------------//
 #pragma once
 
@@ -11,39 +11,43 @@ class TitleSceneSlime;
 class FileManager;
 
 /**
-	@brief	タイトルシーンクラス
+    @brief	タイトルシーンクラス
 */
 class TitleScene : public SceneBase
 {
 public:
-	//-----------------------------------------------------------------------------//
-	//							パブリック関数
-	//-----------------------------------------------------------------------------//
     TitleScene();
-    ~TitleScene()override;
-
+    ~TitleScene()       override;
+    /**
+        @brief	現在のシーンのアップデート処理をまとめて行う
+        @return 次のフレームで実行するシーンのインスタンス
+    */
     SceneBase* Update() override;
-    void Draw() override;
+    /**
+        @brief	現在のシーンの描画処理
+    */
+    void Draw()         override;
 private:
-	//-----------------------------------------------------------------------------//
-	//							プライベート関数
-	//-----------------------------------------------------------------------------//
+    /**
+        @brief	表示している画像のアニメーション処理
+    */
 	void GraphAnimation();
-	void PlayingMovie();
-	//-----------------------------------------------------------------------------//
-	//							プライベート変数
-	//-----------------------------------------------------------------------------//
-	InputManager* inputManager;
-	FileManager* fileManager;
-	int titleGraph;
-	int pushGraph;
-	int opacity;									//不透明度
-	int changingOpacityValue;
-	int demoPlayMovie;
-	int playcount;
-	bool playMovie;
-	bool SceneChangeFlag;
-	bool needPlayingMovie;
-	const int OPACITY_MAX = 80;						//タイトルでアニメーションさせる画像の不透明度の上限値（0~255）
-	const int playMovieUpToTime = 60 * 5;			//デモプレイムービーを流すまでの時間
+    /**
+        @brief	デモムービーを流す処理
+    */
+	void PlayMovie();
+
+	InputManager    *inputManager;                  //入力管理クラスのポインタ
+	FileManager     *fileManager;                   //ファイル管理クラスのポインタ
+	int             titleBackGroundGraph;           //タイトルの背景画像
+	int             pushStartGraph;                 //PushStartの画像
+	int             pushStartOpacity;               //不透明度
+	int             changingOpacityValue;
+	int             demoPlayMovie;
+	int             playcount;
+	bool            playMovie;
+	bool            SceneChangeFlag;
+	bool            needPlayingMovie;
+	const int       OPACITY_MAX = 80;                     //タイトルでアニメーションさせる画像の不透明度の上限値（0~255）
+	const int       playMovieUpToTime = 60 * 5;           //デモプレイムービーを流すまでの時間
 };
